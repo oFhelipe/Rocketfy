6e9af32bf774bb0fb099bc33160365993369daa0
oFhelipe <fhelipe.rodrigues@fatec.sp.gov.br>
2021-07-06 20:00:01 -0300

    finalização

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,23 +1,64 @@
-import React, { useRef } from 'react';
+import React, { useRef, useContext } from 'react';
 import { Container, Label } from './styles';
 import { useDrag, useDrop } from 'react-dnd'
+import BoardContext from '../Board/context'
 
-const Card = ({ data, index }) => {
+const Card = ({ data, index, listIndex }) => {
 
   const ref = useRef()
+  const { move } = useContext(BoardContext)
 
+  //card que segura
   const [{ isDragging }, dragRef] = useDrag({
     type: 'CARD',
-    item: { type: 'CARD', index },
+    item: { type: 'CARD', index, listIndex },
     collect: monitor => ({
       isDragging: monitor.isDragging(),
     })
   })
 
+  //card que o mouse fica em cima
   const [ ,dropRef] = useDrop({
     accept: 'CARD',
     hover (item, monitor) {
+
+      //id da lista que está saindo, lista de origem
+      const draggedListIndex = item.listIndex;
+
+      // id da lista que está recebendo
+      const targetListIndex = listIndex;
+
+      const draggedIndex = item.index;
+      const targetIndex = index;
+
+      if (draggedIndex === targetIndex && draggedListIndex === targetListIndex) {
+        return;
+      }
+      //getBoundingClientRectpega a posição do item
+      const targetSize = ref.current.getBoundingClientRect()
+      const targetCenter = (targetSize.bottom - targetSize.top) / 2
+
+      //pega posição do mouse na tela X e Y
+      const draggedOffset = monitor.getClientOffset()
+
+      //a distancia do elemento segurado do topo da tela 
+      // - 
+      //a distancia do elento hover do topo da tela
+      //resulta em quanto que um card entrou no outro
+      const draggedTop = draggedOffset.y - targetSize.top
+
+      if (draggedIndex < targetIndex && draggedTop < targetCenter && draggedListIndex === targetListIndex) {
+        return
+      }
       
+      if (draggedIndex > targetIndex && draggedTop > targetCenter && draggedListIndex === targetListIndex) {
+        return
+      }
+
+
+      move(draggedIndex, targetIndex, draggedListIndex, targetListIndex)
+      item.index = targetIndex
+      item.listIndex = targetListIndex
     }
   })
 
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useContext } from 'react'
+import { Container } from './styles'
 import { MdAdd } from 'react-icons/md'
 import Card from '../Card'
+import { useDrop } from 'react-dnd'
+import BorderContext from '../Board/context'
 
-const List = ({ data }) => {
+const List = ({ data, index: listIndex }) => {
+  const { move } = useContext(BorderContext)
+
+  const [, dropRef] = useDrop({
+    accept: 'CARD',
+    hover (item) {
+      if (!data.cards[0]) {
+        move(item.index, 0, item.listIndex, listIndex)
+        item.index = 0
+        item.listIndex = listIndex
+      }
+    }
+  })
+
   return (
-    <Container done={data.done}>
+    <Container done={data.done} ref={dropRef}>
       <header>
         <h2>{data.title}</h2>
         {data.creatable && (
-          <button type="button">
-            <MdAdd size={24} color="#FFF"/>
+          <button type='button'>
+            <MdAdd size={24} color='#FFF' />
           </button>
         )}
       </header>
       <ul>
-        {data.cards.map((card, index) => <Card key={card.id} index={index} data={card}/>)}
+        {data.cards.map((card, index) => (
+          <Card key={card.id} listIndex={listIndex} index={index} data={card} />
+        ))}
       </ul>
     </Container>
-  );
+  )
 }
 
-export default List;
+export default List
